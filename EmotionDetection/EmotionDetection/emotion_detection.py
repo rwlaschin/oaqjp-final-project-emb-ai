ba29868e118adfadffc3322001fd8ccaf32b19a8
Robert Wlaschin <raw77_m@yahoo.com>
2024-11-21 03:14:38 -0500

    Step 8

--- EmotionDetection/EmotionDetection/emotion_detection.py
+++ EmotionDetection/EmotionDetection/emotion_detection.py
@@ -1,14 +1,19 @@
+"""
+Uses WatsonX to provide sentiment analysis
+"""
+
 import requests
 import json
 import http
 
 def format_output(emotion_data):
+    """ Formats watsonx response """
     predictions = emotion_data.get("emotionPredictions",[])[0]
     emotion_response = predictions.get("emotion")
     dominant_emotion = { "name": None, "value": None}
 
     for (key,value) in emotion_response.items():
-        if dominant_emotion["value"] == None or value >= dominant_emotion["value"]:
+        if dominant_emotion["value"] is None or value >= dominant_emotion["value"]:
             dominant_emotion["name"] = key
             dominant_emotion["value"] = value
 
@@ -17,6 +22,13 @@ def format_output(emotion_data):
     
 
 def emotion_detector(text_to_analyze):
+    """ 
+        Runs sentiment analysis and formats the response
+        returns: 
+            200, valid text string and object
+            400, invalid text string and Null object
+    """
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
@@ -33,6 +45,9 @@ def emotion_detector(text_to_analyze):
         raise http.HTTPError(response.status_code, response.text)
 
 def pprint(data):
+    """ 
+        Helper to format json object
+    """
     print(json.dumps(data, indent=2))
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,25 +1,38 @@
-from flask import Flask, request, jsonify, render_template
+"""
+Uses WatsonX to provide sentiment analysis
+"""
+
+from flask import Flask, request, render_template
 from EmotionDetection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Displays index page
+    """
     return render_template("index.html")
 
 @app.route('/emotionDetector', methods=['GET'])
-def post_Emotion_Detector():
+def post_emotion_detector():
+    """
+    Fetches response from sentiment analysis and returns formated response
+    """
     text = request.args.get('textToAnalyze')
     print('arg', text)
 
     response = emotion_detector(text)
     print(response)
 
-    if response.get("dominant_emotion") == None:
+    if response.get("dominant_emotion") is None:
         response_str = 'Invalid text!  Please try again!'
     else:
         response_str = 'For the given statement, the system response is ' + \
-            ', '.join([ f"'{key}': {value}" for (key,value) in response.items() if key != 'dominant_emotion' ]) + \
+            ', '.join([
+                f"'{key}': {value}" for (key,value) in response.items()
+                    if key != 'dominant_emotion'
+                ]) + \
             f". The dominant emotion is <strong>{response['dominant_emotion']}</strong>."
 
     return response_str, 200
